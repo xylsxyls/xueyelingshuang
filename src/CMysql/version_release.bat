4c3ebec029176b00ab5e5679d56797a0c0e3449c
xylsxyls <yangnan123456789@163.com>
2016-10-24 08:56:28 +0800

    CMysql生产dll

---
+++ src/CMysql/version_release.bat
@@ -0,0 +1,31 @@
+::release-------------------------------------------------------------------------
+
+set bat=%~dp0
+set xueyelingshuang=%bat%..\..\
+
+::如果需要获取依赖dll-----------------------------------------------------------
+call %xueyelingshuang%src\..relyproj..\version_release.bat
+
+::call完之后都需要重新set
+set bat=%~dp0
+set xueyelingshuang=%bat%..\..\
+::如果需要获取依赖dll-----------------------------------------------------------
+
+::获取第三方库
+call "%bat%public.bat"
+
+::call完之后都需要重新set
+set bat=%~dp0
+set xueyelingshuang=%bat%..\..\
+
+::编译工程
+call "%bat%scripts\rebuild_CMysql.py"
+
+::call完之后都需要重新set
+set bat=%~dp0
+set xueyelingshuang=%bat%..\..\
+
+::向公共部分提供文件，有的时候可能没有inl文件
+xcopy /y /i /r /s "%bat%CMysql\src\*.h"   "%xueyelingshuang%include\CMysql\"
+xcopy /y /i /r /s "%bat%CMysql\src\*.inl" "%xueyelingshuang%include\CMysql\"
+xcopy /y /i /r /s "%bat%CMysql\props\*.props" "%xueyelingshuang%props\"
