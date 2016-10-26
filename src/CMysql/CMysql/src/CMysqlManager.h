d4d899206085311929654533ca73e4cb3c535a04
xylsxyls <yangnan123456789@163.com>
2016-10-26 23:12:43 +0800

    修改

--- src/CMysql/CMysql/src/CMysqlManager.h
+++ src/CMysql/CMysql/src/CMysqlManager.h
@@ -8,9 +8,19 @@ class CUser;
 class CDataBase;
 class CTable;
 
+template class CMysqlAPI std::allocator<CIP      *>;
+template class CMysqlAPI std::allocator<CUser    *>;
+template class CMysqlAPI std::allocator<CDataBase*>;
+template class CMysqlAPI std::allocator<CTable   *>;
+template class CMysqlAPI std::list<CIP      *,std::allocator<CIP      *>>;
+template class CMysqlAPI std::list<CUser    *,std::allocator<CUser    *>>;
+template class CMysqlAPI std::list<CDataBase*,std::allocator<CDataBase*>>;
+template class CMysqlAPI std::list<CTable   *,std::allocator<CTable   *>>;
+
 class CMysqlAPI CMysqlManager{
 	//所有当前CMysql下开的内存全部会放在管理者的向量中，管理者一旦被删除则所有内存释放
 public:
+	
 	//用来记录内存开辟情况，所有开辟的内存地址都会在这里记录
 	list<CIP      *> listIP      ;
 	list<CUser    *> listUser    ;
