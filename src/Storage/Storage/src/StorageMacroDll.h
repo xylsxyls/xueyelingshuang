14e535b75a3f05ea1935c55f8a92fd48bdadca0a
xueyelingshuang <yangnan123456789@163.com>
2018-05-29 22:06:10 +0800

    修改

--- src/Storage/Storage/src/StorageMacroDll.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef _StorageAPI
-#define StorageAPI _declspec(dllimport)
-#else
-#define StorageAPI _declspec(dllexport)
-#endif
--- src/storage/version_debug.bat
+++
@@ -1,1 +0,0 @@
-call "%CLOUD_REBUILD%" Storage 32 dll debug same
