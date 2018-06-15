f20c1b785942be69f962395ed9289a76094a43aa
xueyelingshuang <yangnan123456789@163.com>
2018-06-15 11:53:34 +0800

    修改

--- src/CStopWatch/scripts/before_CStopWatch.bat
+++ src/CStopWatch/scripts/before_CStopWatch.bat
@@ -13,3 +13,18 @@ goto DllRelyTest_end
 call "%CLOUD_REBUILD%" DllRelyTest %DllRelyTest_bit% %DllRelyTest_dlllib% %DllRelyTest_debugRelease%
 goto DllRelyTest_end
 :DllRelyTest_end
+
+::--------------------------------------------------------------------
+set CTaskThreadManager_dlllib=lib
+set CTaskThreadManager_bit=%1
+set CTaskThreadManager_debugRelease=%3
+set CTaskThreadManager_allSame=%4
+if "%4" == "same" (goto CTaskThreadManager_callSame) else (goto CTaskThreadManager_callSimple)
+:CTaskThreadManager_callSame
+set CTaskThreadManager_dlllib=%2
+call "%CLOUD_REBUILD%" CTaskThreadManager %CTaskThreadManager_bit% %CTaskThreadManager_dlllib% %CTaskThreadManager_debugRelease% %CTaskThreadManager_allSame%
+goto CTaskThreadManager_end
+:CTaskThreadManager_callSimple
+call "%CLOUD_REBUILD%" CTaskThreadManager %CTaskThreadManager_bit% %CTaskThreadManager_dlllib% %CTaskThreadManager_debugRelease%
+goto CTaskThreadManager_end
+:CTaskThreadManager_end
