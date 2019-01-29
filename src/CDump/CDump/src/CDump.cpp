2ca4ec3fe4cb1b475d956c5091226152dfc7d551
xueyelingshuang <yangnan123456789@163.com>
2019-01-29 11:51:53 +0800

    修改dump生成

--- src/CDump/CDump/src/CDump.cpp
+++ src/CDump/CDump/src/CDump.cpp
@@ -2,92 +2,80 @@
 #include <tchar.h>
 #include <Windows.h>
 #include <Dbghelp.h>
-#include "CGetPath/CGetPathAPI.h"
-#pragma comment(lib,"Dbghelp.lib")
+#include "CSystem/CSystemAPI.h"
 
-void CreateDumpFile(LPCSTR lpstrDumpFilePathName, EXCEPTION_POINTERS *pException)
+std::string g_dumpFilePath;
+
+int GenerateMiniDump(PEXCEPTION_POINTERS pExceptionPointers)
 {
-    //创建Dump文件  
-    HANDLE hDumpFile = ::CreateFile(lpstrDumpFilePathName, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-    //Dump信息
-    MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
-    dumpInfo.ExceptionPointers = pException;
-    dumpInfo.ThreadId = ::GetCurrentThreadId();
-    dumpInfo.ClientPointers = TRUE;
-    //写入Dump文件内容  
-    ::MiniDumpWriteDump(::GetCurrentProcess(), ::GetCurrentProcessId(), hDumpFile, MiniDumpNormal, &dumpInfo, nullptr, nullptr);
-    ::CloseHandle(hDumpFile);
-    return;
+	// 定义函数指针
+	typedef BOOL(WINAPI * MiniDumpWriteDumpT)(
+		HANDLE,
+		DWORD,
+		HANDLE,
+		MINIDUMP_TYPE,
+		PMINIDUMP_EXCEPTION_INFORMATION,
+		PMINIDUMP_USER_STREAM_INFORMATION,
+		PMINIDUMP_CALLBACK_INFORMATION
+		);
+	// 从 "DbgHelp.dll" 库中获取 "MiniDumpWriteDump" 函数
+	MiniDumpWriteDumpT pfnMiniDumpWriteDump = NULL;
+	HMODULE hDbgHelp = LoadLibrary(_T("DbgHelp.dll"));
+	if (NULL == hDbgHelp)
+	{
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	pfnMiniDumpWriteDump = (MiniDumpWriteDumpT)GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
+
+	if (NULL == pfnMiniDumpWriteDump)
+	{
+		FreeLibrary(hDbgHelp);
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	// 创建 dmp 文件件
+	HANDLE hDumpFile = CreateFile(g_dumpFilePath.c_str(), GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+	if (INVALID_HANDLE_VALUE == hDumpFile)
+	{
+		FreeLibrary(hDbgHelp);
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	// 写入 dmp 文件
+	MINIDUMP_EXCEPTION_INFORMATION expParam;
+	expParam.ThreadId = GetCurrentThreadId();
+	expParam.ExceptionPointers = pExceptionPointers;
+	expParam.ClientPointers = FALSE;
+	pfnMiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
+		hDumpFile, MiniDumpWithDataSegs, (pExceptionPointers ? &expParam : NULL), NULL, NULL);
+	// 释放文件
+	CloseHandle(hDumpFile);
+	FreeLibrary(hDbgHelp);
+	return EXCEPTION_EXECUTE_HANDLER;
 }
 
-LPTOP_LEVEL_EXCEPTION_FILTER WINAPI MyDummySetUnhandledExceptionFilter(LPTOP_LEVEL_EXCEPTION_FILTER lpTopLevelExceptionFilter)
+LONG WINAPI ExceptionFilter(LPEXCEPTION_POINTERS lpExceptionInfo)
 {
-    return nullptr;
+	// 这里做一些异常的过滤或提示
+	if (IsDebuggerPresent())
+	{
+		return EXCEPTION_CONTINUE_SEARCH;
+	}
+	return GenerateMiniDump(lpExceptionInfo);
 }
 
-BOOL PreventSetUnhandledExceptionFilter()
+bool CDump::declareDumpFile(const std::string& dumpFileDir)
 {
-    HMODULE hKernel32 = ::LoadLibrary(_T("kernel32.dll"));
-    if (hKernel32 == nullptr)
-    {
-        return FALSE;
-    }
-    void* pOrgEntry = ::GetProcAddress(hKernel32, "SetUnhandledExceptionFilter");
-    if (pOrgEntry == nullptr)
-    {
-        return FALSE;
-    }
-    unsigned char newJump[100] = {};
-    DWORD dwOrgEntryAddr = (DWORD)pOrgEntry;
-    dwOrgEntryAddr += 5; //add 5 for 5 op-codes for jmp far
-    void *pNewFunc = &MyDummySetUnhandledExceptionFilter;
-    DWORD dwNewEntryAddr = (DWORD)pNewFunc;
-    DWORD dwRelativeAddr = dwNewEntryAddr - dwOrgEntryAddr;
-    newJump[0] = 0xE9; //JMP absolute
-    ::memcpy(&newJump[1], &dwRelativeAddr, sizeof(pNewFunc));
-    SIZE_T bytesWritten;
-    BOOL bRet = ::WriteProcessMemory(::GetCurrentProcess(), pOrgEntry, newJump, sizeof(pNewFunc) + 1, &bytesWritten);
-    return bRet;
-}
-
-LONG WINAPI UnhandledExceptionFilterEx(struct _EXCEPTION_POINTERS *pException)
-{
-    CreateDumpFile(CDump::instance().GetDumpFilePath().c_str(), pException);
-    // TODO: MiniDumpWriteDump
-    ::FatalAppExit(-1, _T("Fatal Error"));
-    return EXCEPTION_CONTINUE_SEARCH;
-}
-
-void RunCrashHandler()
-{
-    ::SetUnhandledExceptionFilter(UnhandledExceptionFilterEx);
-    ::PreventSetUnhandledExceptionFilter();
-    return;
-}
-
-CDump::CDump()
-{
-
-}
-
-CDump& CDump::instance()
-{
-    static CDump dump;
-    return dump;
-}
-
-void CDump::DeclareDumpFile(const std::string& dumpFilePath)
-{
-    m_dumpFilePath = dumpFilePath;
-    if (dumpFilePath == "")
-    {
-        m_dumpFilePath = CGetPath::GetCurrentExePath() + CGetPath::GetCurrentExeName() + ".dmp";
-    }
-    RunCrashHandler();
-    return;
-}
-
-std::string CDump::GetDumpFilePath()
-{
-    return m_dumpFilePath;
+	if (g_dumpFilePath != "")
+	{
+		return false;
+	}
+	TCHAR szFileName[MAX_PATH] = { 0 };
+	SYSTEMTIME stLocalTime;
+	GetLocalTime(&stLocalTime);
+	wsprintf(szFileName, "%s_%04d%02d%02d_%02d%02d%02d.dmp",
+		CSystem::GetCurrentExeName().c_str(), stLocalTime.wYear, stLocalTime.wMonth, stLocalTime.wDay,
+		stLocalTime.wHour, stLocalTime.wMinute, stLocalTime.wSecond);
+	g_dumpFilePath = (dumpFileDir.empty() ? CSystem::GetCurrentExePath() : dumpFileDir) + szFileName;
+	SetUnhandledExceptionFilter(ExceptionFilter);
+    return true;
 }
--- src/CDump/CDump/src/CDump.h
+++ src/CDump/CDump/src/CDump.h
@@ -4,25 +4,10 @@
 
 class CDumpAPI CDump
 {
-private:
-    CDump();
-
 public:
-    static CDump& instance();
-
-public:
-	//执行这句之后程序之后如果再出错就会在当前进程下产生dump文件，建议传绝对路径
-	void DeclareDumpFile(const std::string& dumpFilePath = "");
-
-    std::string GetDumpFilePath();
-
-private:
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable:4251)
-#endif
-    std::string m_dumpFilePath;
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+	/** 程序崩溃生成dump文件
+	@param [in] dumpFileDir 生成dump的目录，带\符号，传空则在exe同级目录下创建
+	@return 返回是否执行成功，只有第一次执行会成功
+	*/
+	static bool declareDumpFile(const std::string& dumpFileDir = "");
 };
