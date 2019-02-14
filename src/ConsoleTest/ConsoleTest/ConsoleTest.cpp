540e492050da684016ad8bb07f73d1a703079aa9
xueyelingshuang <yangnan123456789@163.com>
2019-02-14 17:22:13 +0800

    修改

--- src/ConsoleTest/ConsoleTest/ConsoleTest.cpp
+++ src/ConsoleTest/ConsoleTest/ConsoleTest.cpp
@@ -1,10 +1,6 @@
 #include "ConsoleTest.h"
 #include <stdint.h>
 #include <stdio.h>
-#include "LogSender/LogSenderAPI.h"
-#include "LogManager/LogManagerAPI.h"
-#include "ProcessWork/ProcessWorkAPI.h"
-#include "ProtoMessage/ProtoMessageAPI.h"
 
 BOOL CALLBACK ConsoleHandler(DWORD eve)
 {
@@ -20,21 +16,7 @@ int32_t consoleCloseResult = ::SetConsoleCtrlHandler(ConsoleHandler, TRUE);
 
 int32_t main()
 {
-	//RCSend("time = %d", ::GetTickCount());
-	int32_t count = 25000;
-	while (count-- != 0)
-	{
-		LOG_SEND_ALL("%d，%s", 123456, "我们");
-		LOG_SEND_ALL("%d，%s", 1234567, "我们说");
-		LOG_SEND_ALL("%d，%s", 12345678, "我们说来");
-		LOG_SEND_ALL("%d，%s", 123456789, "我们说来了");
-		if (count % 10000 == 0)
-		{
-			//RCSend("count last = %d", count);
-		}
-	}
-	//RCSend("end,time = %d", ::GetTickCount());
-
+	
 	getchar();
 	return 0;
 }
