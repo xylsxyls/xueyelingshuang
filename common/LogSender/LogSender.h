29b1806a0547a3041a46a418e063f7c4e7014e43
xueyelingshuang <yangnan123456789@163.com>
2019-02-18 21:19:04 +0800

    添加

--- common/LogSender/LogSender.h
+++ common/LogSender/LogSender.h
@@ -1,17 +1,45 @@
 #pragma once
 #include "LogSenderMacro.h"
 #include <stdint.h>
+#include "LogPackage.h"
+
+#define LOG_SEND_ALL(format, ...)  LogSender::instance().logSend(LogPackage(LogPackage::LOG_INFO, __FILE__, __FUNCTION__, true, true, true), format, ##__VA_ARGS__)
+#define LOG_SEND_DEBUG(format, ...) LogSender::instance().logSend(LogPackage(LogPackage::LOG_DEBUG, __FILE__, __FUNCTION__, false, true, true), format, ##__VA_ARGS__)
+#define LOG_SEND_INFO(format, ...) LogSender::instance().logSend(LogPackage(LogPackage::LOG_INFO, __FILE__, __FUNCTION__, false, true, true), format, ##__VA_ARGS__)
+#define LOG_SEND_WARNING(format, ...) LogSender::instance().logSend(LogPackage(LogPackage::LOG_WARNING, __FILE__, __FUNCTION__, true, true, true), format, ##__VA_ARGS__)
+#define LOG_SEND_ERROR(format, ...) LogSender::instance().logSend(LogPackage(LogPackage::LOG_ERROR, __FILE__, __FUNCTION__, true, true, true), format, ##__VA_ARGS__)
+#define LOG_SEND_FATAL(format, ...) LogSender::instance().logSend(LogPackage(LogPackage::LOG_FATAL, __FILE__, __FUNCTION__, true, true, true), format, ##__VA_ARGS__)
+
+class ProtoMessage;
+class SharedMemory;
 
 class LogSenderAPI LogSender
 {
 protected:
 	LogSender();
 
+	~LogSender();
+
 public:
 	static LogSender& instance();
 
 public:
-	void send(char* buffer, int32_t length);
+	void logSend(const LogPackage& package, const char* format, ...);
 
 	void uninit();
+
+protected:
+	void send(const char* buffer, int32_t length);
+
+private:
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4251)
+#endif
+	std::string m_computerName;
+	ProtoMessage* m_message;
+	SharedMemory* m_logTestPid;
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 };
