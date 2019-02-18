9b99930ffff29c32b66f278aa5a1825250d856ab
xueyelingshuang <yangnan123456789@163.com>
2019-02-18 14:18:10 +0800

    修改

--- src/Cini/Cini/src/Cini.cpp
+++ src/Cini/Cini/src/Cini.cpp
@@ -2,83 +2,96 @@
 #include <windows.h>
 #include <vector>
 
-Cini::Cini(const string& iniPath){
-	this->iniPath = iniPath;
+Cini::Cini(const std::string& iniPath)
+{
+	m_iniPath = iniPath;
 }
 
-string Cini::ReadIni(const string& key, const string& strSection, int MaxLength)const
+std::string Cini::ReadIni(const std::string& key, const std::string& strSection, int32_t maxLength) const
 {
-	char *szText = new char[MaxLength];
-	GetPrivateProfileString(strSection.c_str(),key.c_str(),"-1",szText,MaxLength,iniPath.c_str());
-	string strText = szText;
-	delete[] szText;
-	return strText;
+	if (maxLength == 0)
+	{
+		return "";
+	}
+	std::string text;
+	text.resize(maxLength);
+	GetPrivateProfileString(strSection.c_str(), key.c_str(), "", &text[0], maxLength, m_iniPath.c_str());
+	text.resize(strlen(&text[0]));
+	return text;
 }
 
-void Cini::WriteIni(const string& key, const string& text, const string& strSection)const
+void Cini::WriteIni(const std::string& key, const std::string& text, const std::string& strSection) const
 {
-	WritePrivateProfileString(strSection.c_str(),key.c_str(),text.c_str(),iniPath.c_str());
-	return;
+	WritePrivateProfileString(strSection.c_str(), key.c_str(), text.c_str(), m_iniPath.c_str());
 }
 
-std::vector<string> Cini::GetSection(int MaxSectionNum, int MaxLength)const
+std::vector<std::string> Cini::GetSection(int32_t maxSectionNum, int32_t maxLength) const
 {
-    std::vector<string> vecSection;
-    int iLength = 0;
-    int iPos = 0;
-    //总长度
-    unsigned int MaxSectionLength = (unsigned int)MaxSectionNum * MaxLength;
-    //总的提出来的字符串
-    TCHAR* chSectionNames = new TCHAR[MaxSectionLength];
-    //存放一个段名
-    TCHAR* chSection = new TCHAR[MaxLength];
-    DWORD nChar = GetPrivateProfileSectionNames(chSectionNames, MaxSectionLength, iniPath.c_str());
-    //以下循环，截断到两个连续的0 
-    for (int i = 0; (DWORD)i < nChar; i++)
-    {
-        if (chSectionNames[i] == 0)
-        {
+	std::vector<std::string> vecSection;
+	int32_t iLength = 0;
+	int32_t iPos = 0;
+	//总长度
+	uint32_t maxSectionLength = (uint32_t)maxSectionNum * (uint32_t)maxLength;
+	//总的提出来的字符串
+	TCHAR* chSectionNames = new TCHAR[maxSectionLength];
+	//存放一个段名
+	TCHAR* chSection = new TCHAR[maxLength];
+	DWORD nChar = GetPrivateProfileSectionNames(chSectionNames, maxSectionLength, m_iniPath.c_str());
+	//以下循环，截断到两个连续的0
+	for (int32_t i = 0; (DWORD)i < nChar; i++)
+	{
+		if (chSectionNames[i] == 0)
+		{
 			chSection[iLength] = 0;
-            vecSection.push_back(chSection);
-            if (chSectionNames[i] == chSectionNames[i + 1])
-            {
-                break;
-            }
-            memset(chSection, 0, iLength);
-            iPos = 0;
-            iLength = 0;
-        }
-        else
-        {
-            chSection[iPos++] = chSectionNames[i];
-            iLength++;
-        }
-    }
-    delete[] chSectionNames;
-    delete[] chSection;
-    return vecSection;
+			vecSection.push_back(chSection);
+			if (chSectionNames[i] == chSectionNames[i + 1])
+			{
+				break;
+			}
+			memset(chSection, 0, iLength);
+			iPos = 0;
+			iLength = 0;
+		}
+		else
+		{
+			chSection[iPos++] = chSectionNames[i];
+			iLength++;
+		}
+	}
+	delete[] chSectionNames;
+	delete[] chSection;
+	return vecSection;
 }
 
-string Cini::GetFirstKey(const string& section, int MaxLength)const
+std::string Cini::GetFirstKey(const std::string& section, int32_t maxLength) const
 {
-    TCHAR* strBuffer = new TCHAR[MaxLength];
-    GetPrivateProfileString(section.c_str(), NULL, "-1", strBuffer, MaxLength, iniPath.c_str());
-    string result = strBuffer;
-    delete[] strBuffer;
-    return result;
+	std::string result;
+	result.resize(maxLength);
+	GetPrivateProfileString(section.c_str(), nullptr, "-1", &result[0], maxLength, m_iniPath.c_str());
+	result.resize(strlen(&result[0]));
+	return result;
 }
 
-void Cini::DeleteKey(const string& key, const string& section)const
+void Cini::DeleteKey(const std::string& key, const std::string& section) const
 {
-    WritePrivateProfileString(section.c_str(), key.c_str(), NULL, iniPath.c_str());
+	WritePrivateProfileString(section.c_str(), key.c_str(), nullptr, m_iniPath.c_str());
 }
 
-void Cini::DeleteSection(const string& section)const
+void Cini::DeleteSection(const std::string& section) const
 {
-    WritePrivateProfileSection(section.c_str(), NULL, iniPath.c_str());
+	WritePrivateProfileSection(section.c_str(), nullptr, m_iniPath.c_str());
 }
 
-int main(){
-    Cini ini("D:\\xueyelingshuang\\lib\\save.data");
-    return 0;
-}
+//int32_t main()
+//{
+//	Cini ini("D:\\xueyelingshuang\\lib\\save.ini");
+//	ini.WriteIni("sad", "1234a");
+//	std::string str = ini.ReadIni("sad");
+//	printf("str = %s, len = %d\n", str.c_str(), str.length());
+//	std::string key = ini.GetFirstKey();
+//	printf("key = %s\n", key.c_str());
+//	std::vector<std::string> vecSection = ini.GetSection();
+//	printf("vec.size = %d, vec[0] = %s\n", vecSection.size(), vecSection[0].c_str());
+//	getchar();
+//	return 0;
+//}
--- src/Cini/Cini/src/Cini.h
+++ src/Cini/Cini/src/Cini.h
@@ -1,40 +1,40 @@
 #pragma once
-#include "CiniMacro.h"
 #include <string>
 #include <vector>
-using namespace std;
+#include <stdint.h>
+#include "CiniMacro.h"
 
-class CiniAPI Cini{
+class CiniAPI Cini
+{
 public:
+	Cini(const std::string& iniPath);
 
+public:
+	//取出的信息默认1024最大长度，如果key不存在，默认返回"-1"，路径传绝对路径，strSection默认为Section
+	std::string ReadIni(const std::string& key, const std::string& strSection = "Section", int32_t maxLength = 1024) const;
+
+	//路径传绝对路径，strSection默认为Section，key不存在则创建，存在则修改，文件可以不存在路径必须存在
+	void WriteIni(const std::string& key, const std::string& text, const std::string& strSection = "Section") const;
+
+    //获取所有Section，传入最大section个数
+	std::vector<std::string> GetSection(int32_t maxSectionNum = 1024, int32_t maxLength = 1024) const;
+
+    //获取该section下第一个key名
+	std::string GetFirstKey(const std::string& section = "Section", int32_t maxLength = 1024) const;
+
+    //删除该section下的指定key
+	void DeleteKey(const std::string& key, const std::string& section = "Section") const;
+
+    //删除指定section
+	void DeleteSection(const std::string& section = "Section") const;
+
+private:
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable:4251)
 #endif
-	string iniPath;
+	std::string m_iniPath;
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif
-	
-public:
-	Cini(const string& iniPath);
-
-public:
-	//取出的信息默认1024最大长度，如果key不存在，默认返回"-1"，路径传绝对路径，strSection默认为Section
-	string ReadIni(const string& key, const string& strSection = "Section", int MaxLength = 1024)const;
-
-	//路径传绝对路径，strSection默认为Section，key不存在则创建，存在则修改，文件可以不存在路径必须存在
-	void WriteIni(const string& key, const string& text, const string& strSection = "Section")const;
-
-    //获取所有Section，传入最大section个数
-	vector<string> GetSection(int MaxSectionNum, int MaxLength = 1024)const;
-
-    //获取该section下第一个key名
-	string GetFirstKey(const string& section = "Section", int MaxLength = 1024)const;
-
-    //删除该section下的指定key
-	void DeleteKey(const string& key, const string& section = "Section")const;
-
-    //删除指定section
-	void DeleteSection(const string& section = "Section")const;
 };
