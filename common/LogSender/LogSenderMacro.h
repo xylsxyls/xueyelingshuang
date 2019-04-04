2ad2e7d448e662a7c803e5225f4e8b34b0f86efb
xueyelingshuang <yangnan123456789@163.com>
2019-04-04 14:46:45 +0800

    添加

--- common/LogSender/LogSenderMacro.h
+++ common/LogSender/LogSenderMacro.h
@@ -1,12 +1,3 @@
 #pragma once
 
-#ifdef _LogSenderAPI
-#define LogSenderAPI _declspec(dllimport)
-#else
-#define LogSenderAPI _declspec(dllexport)
-#endif
-
-#if defined(STATIC_LIB)
-#undef LogSenderAPI
 #define LogSenderAPI 
-#endif
