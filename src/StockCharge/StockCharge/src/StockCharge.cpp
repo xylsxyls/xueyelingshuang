04755043fa8274310392608e20929f331425ba03
xueyelingshuang <yangnan123456789@163.com>
2019-04-27 03:30:09 +0800

    修改

--- src/StockCharge/StockCharge/src/StockCharge.cpp
+++ src/StockCharge/StockCharge/src/StockCharge.cpp
@@ -1,1 +1,50 @@
 #include "StockCharge.h"
+
+StockCharge::StockCharge()
+{
+	m_rate = 25;
+	m_rate = m_rate / 100000;
+	m_minimum = 5;
+}
+
+StockCharge& StockCharge::instance()
+{
+	static StockCharge s_stockCharge;
+	return s_stockCharge;
+}
+
+void StockCharge::init(const BigNumber& rate, const BigNumber& minimum)
+{
+	m_rate = rate;
+	m_minimum = minimum;
+}
+
+BigNumber StockCharge::buyFee(const BigNumber& price, const BigNumber& position)
+{
+	return transferFee(price, position) + commission(price, position);
+}
+
+BigNumber StockCharge::sellFee(const BigNumber& price, const BigNumber& position)
+{
+	return stampDuty(price, position) + commission(price, position);
+}
+
+BigNumber StockCharge::stampDuty(const BigNumber& price, const BigNumber& position)
+{
+	return (price * position / 1000).toPrec(2);
+}
+
+BigNumber StockCharge::transferFee(const BigNumber& price, const BigNumber& position)
+{
+	return (price * position * 2 / 100000).toPrec(2);
+}
+
+BigNumber StockCharge::commission(const BigNumber& price, const BigNumber& position)
+{
+	BigNumber commissionFee = price * position * m_rate;
+	if (commissionFee < m_minimum)
+	{
+		return m_minimum;
+	}
+	return commissionFee.toPrec(2);
+}
--- src/StockCharge/StockCharge/src/StockCharge.h
+++ src/StockCharge/StockCharge/src/StockCharge.h
@@ -1,8 +1,68 @@
 #pragma once
 #include "StockChargeMacro.h"
+#include "BigNumber/BigNumberAPI.h"
 
+/** 股票交易时税收计算类
+*/
 class StockChargeAPI StockCharge
 {
+protected:
+	/** 构造函数
+	*/
+	StockCharge();
+
 public:
-	
+	/** 单一实例
+	@return 返回单一实例
+	*/
+	static StockCharge& instance();
+
+public:
+	/** 初始化佣金，默认万2.5，最低5元
+	@param [in] rate 佣金比例
+	@param [in] minimum 最低佣金
+	*/
+	void init(const BigNumber& rate, const BigNumber& minimum);
+
+	/** 获取买入时候需要交的税
+	@param [in] price 股票价格
+	@param [in] position 卖出仓位
+	@return 返回买入时候需要交的税
+	*/
+	BigNumber buyFee(const BigNumber& price, const BigNumber& position);
+
+	/** 获取卖出时候需要交的税
+	@param [in] price 股票价格
+	@param [in] position 卖出仓位
+	@return 返回卖出时候需要交的税
+	*/
+	BigNumber sellFee(const BigNumber& price, const BigNumber& position);
+
+protected:
+	/** 获取卖出时需要的印花税，四舍五入到分
+	@param [in] price 股票价格
+	@param [in] position 卖出仓位
+	@return 返回需要交的印花税
+	*/
+	BigNumber stampDuty(const BigNumber& price, const BigNumber& position);
+
+	/** 过户费，购买时需要交，四舍五入到分
+	@param [in] price 股票价格
+	@param [in] position 卖出仓位
+	@return 返回需要交的过户费
+	*/
+	BigNumber transferFee(const BigNumber& price, const BigNumber& position);
+
+	/** 佣金，双向收取，四舍五入到分
+	@param [in] price 股票价格
+	@param [in] position 卖出仓位
+	@return 返回需要交的佣金
+	*/
+	BigNumber commission(const BigNumber& price, const BigNumber& position);
+
+protected:
+	//佣金比例
+	BigNumber m_rate;
+	//最低佣金
+	BigNumber m_minimum;
 };
