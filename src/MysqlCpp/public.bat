dffddc62264a573e32cf5401bb645eb695d37cb0
yangnan <yangnan@sensetime.com>
2019-07-02 10:22:58 +0800

    修改mysqlcpp拷贝脚本

--- src/MysqlCpp/public.bat
+++ src/MysqlCpp/public.bat
@@ -5,9 +5,16 @@ xcopy /y /i /r /s "%xueyelingshuang%tools\mysqlcpp\include\*" "%xueyelingshuang%
 
 if "%1" == "32" (goto MysqlCpp_32) else (goto MysqlCpp_64)
 :MysqlCpp_32
+if "%2" == "release" (goto MysqlCpp_release) else (goto MysqlCpp_debug)
+:MysqlCpp_release
 xcopy /y /i /r /s "%xueyelingshuang%tools\mysqlcpp\lib\release\*" "%xueyelingshuang%lib\"
 goto MysqlCpp_end
 :MysqlCpp_64
 xcopy /y /i /r /s "%xueyelingshuang%tools\mysqlcpp\lib64\*" "%xueyelingshuang%lib\"
 goto MysqlCpp_end
 :MysqlCpp_end
+goto MysqlCpp_debug_release_end
+:MysqlCpp_debug
+xcopy /y /i /r /s "%xueyelingshuang%tools\mysqlcpp\lib\debug\*" "%xueyelingshuang%lib\"
+goto MysqlCpp_debug_release_end
+:MysqlCpp_debug_release_end
