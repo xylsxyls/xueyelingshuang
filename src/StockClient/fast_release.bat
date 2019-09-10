f4dc7fad154062277db658e7a99c08827e14c2e6
yangnan <yangnan@sensetime.com>
2019-09-10 21:59:01 +0800

    添加

---
+++ src/StockClient/fast_release.bat
@@ -0,0 +1,7 @@
+call "%CLOUD_REBUILD%" FastNumber 32 dll release same
+call "%CLOUD_REBUILD%" StockMysql 32 dll release only
+call "%CLOUD_REBUILD%" StockCharge 32 dll release only
+call "%CLOUD_REBUILD%" StockMarket 32 dll release only
+call "%CLOUD_REBUILD%" StockIndicator 32 dll release only
+call "%CLOUD_REBUILD%" StockDraw 32 dll release only
+call "%CLOUD_REBUILD%" StockFund 32 dll release only
