5bbe920fac1841fd852eeea510396080e8e785d1
xueyelingshuang <yangnan123456789@163.com>
2020-01-16 20:22:30 +0800

    增加fast发布

--- src/StockClient/fast_release.bat
+++ src/StockClient/fast_release.bat
@@ -3,8 +3,8 @@ call "%CLOUD_REBUILD%" StockMysql 32 dll release only
 call "%CLOUD_REBUILD%" StockCharge 32 dll release only
 call "%CLOUD_REBUILD%" StockMarket 32 dll release only
 call "%CLOUD_REBUILD%" StockIndicator 32 dll release only
-call "%CLOUD_REBUILD%" StockDraw 32 dll release only
 call "%CLOUD_REBUILD%" StockFund 32 dll release only
+call "%CLOUD_REBUILD%" StockDraw 32 dll release only
 call "%CLOUD_REBUILD%" StockStrategy 32 dll release only
 call "%CLOUD_REBUILD%" StockSolution 32 dll release only
 call "%CLOUD_REBUILD%" StockTrade 32 dll release only
