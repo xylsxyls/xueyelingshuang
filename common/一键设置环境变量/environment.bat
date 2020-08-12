5fde6e122595005ec35438aa2a9a3d6390d4651a
yangnan <yangnan@sensetime.com>
2020-08-12 16:06:24 +0800

    修改

--- common/一键设置环境变量/environment.bat
+++ common/一键设置环境变量/environment.bat
@@ -1,6 +1,6 @@
 setx DEVENV_EXE "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" /m
 setx DEVENV_EXE_2017 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" /m
-setx CLOUD_REBUILD "D:\xueyelingshuang\common\‘∆±‡“Î\CloudRebuild1.2.exe" /m
+setx CLOUD_REBUILD "D:\xueyelingshuang\common\CloudRebuild\CloudRebuild1.2.exe" /m
 setx FILE_REPLACE "D:\xueyelingshuang\common\FileReplace\FileReplace1.0.exe" /m
 setx XUEYELINGSHUANG "D:\xueyelingshuang\\" /m
 setx BOOST_ROOT "D:\boost\boost_1_58_0\\" /m
