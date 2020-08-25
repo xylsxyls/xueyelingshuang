7e69caa02409af7ed7b44ac9ce28b30562692ab1
yangnan <yangnan@sensetime.com>
2020-08-25 17:04:54 +0800

    修改

--- src/OneKey/version_debug.bat
+++
@@ -1,5 +0,0 @@
-call "%CLOUD_REBUILD%" CHook 32 dll debug same
-call "%CLOUD_REBUILD%" CStopWatch 32 dll debug same
-call "%CLOUD_REBUILD%" CKeyBoard 32 dll debug same
-call "%CLOUD_REBUILD%" CMouse 32 dll debug same
-call "%CLOUD_REBUILD%" CTaskThreadManager 32 dll debug same
---
+++ src/OneKey/version_debug.sh
@@ -0,0 +1,5 @@
+"$CLOUD_REBUILD" CHook 32 dll debug same
+"$CLOUD_REBUILD" CStopWatch 32 dll debug same
+"$CLOUD_REBUILD" CKeyBoard 32 dll debug same
+"$CLOUD_REBUILD" CMouse 32 dll debug same
+"$CLOUD_REBUILD" CTaskThreadManager 32 dll debug same
