cf3a2a3d0fab73e631df904d740d27171d8133a4
yangnan <yangnan@sensetime.com>
2020-08-25 16:02:21 +0800

    修改

--- src/NetClientManager/version_debug.bat
+++
@@ -1,5 +0,0 @@
-call "%CLOUD_REBUILD%" ProtoMessage 32 dll debug same
-call "%CLOUD_REBUILD%" NetWork 32 dll debug same
-call "%CLOUD_REBUILD%" ProcessWork 32 dll debug same
-call "%CLOUD_REBUILD%" CDump 32 dll debug same
-call "%CLOUD_REBUILD%" Compress 32 dll debug same
---
+++ src/NetClientManager/version_release.sh
@@ -0,0 +1,5 @@
+"$CLOUD_REBUILD" ProtoMessage 32 lib release same
+"$CLOUD_REBUILD" NetWork 32 lib release same
+"$CLOUD_REBUILD" ProcessWork 32 lib release same
+"$CLOUD_REBUILD" CDump 32 lib release same
+"$CLOUD_REBUILD" Compress 32 lib release same
