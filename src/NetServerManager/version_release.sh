e9fb18f28bb36a4213aa1ce14ab41429e411e2ae
yangnan <yangnan@sensetime.com>
2020-08-25 16:04:57 +0800

    修改

---
+++ src/NetServerManager/version_release.sh
@@ -0,0 +1,5 @@
+"$CLOUD_REBUILD" ProtoMessage 32 dll release same
+"$CLOUD_REBUILD" NetWork 32 dll release same
+"$CLOUD_REBUILD" ProcessWork 32 dll release same
+"$CLOUD_REBUILD" CDump 32 dll release same
+"$CLOUD_REBUILD" Compress 32 dll release same
