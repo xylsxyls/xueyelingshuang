e55b6a0ba5f9c5702972309e4b119a00602e8d9e
xylsxyls <yangnan123456789@163.com>
2021-02-03 04:21:18 -0800

    modify

--- common/linux/shell.sh
+++ common/linux/shell.sh
@@ -3,6 +3,7 @@ chmod 777 ../FileReplace/FileReplace1.1
 chmod 777 ../PerformanceTest/PerformanceTest1.0
 chmod 777 ../ResourceFile/ResourceFile1.0
 chmod 777 ../MessageTest/MessageTest1.0
+chmod 777 ../FindText/FindText1.0
 chmod 777 ../Launch/Launch1.0
 chmod 777 ./*.sh
 chmod 777 ../../*/*/*.sh
