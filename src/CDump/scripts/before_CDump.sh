3cf4d283d17aa8c7121425a5c31c0fef65b2afa3
yangnan <yangnan@sensetime.com>
2021-03-02 09:39:25 +0800

    修改

--- src/CDump/scripts/before_CDump.sh
+++ src/CDump/scripts/before_CDump.sh
@@ -7,17 +7,3 @@ DllRelyTest_debugRelease=$3
 DllRelyTest_allSame=$4
 "$CLOUD_REBUILD" DllRelyTest $DllRelyTest_bit $DllRelyTest_dlllib $DllRelyTest_debugRelease $DllRelyTest_allSame
 !EOF!
-
-#--------------------------------------------------------------------
-CStringManager_bit=$1
-CStringManager_dlllib=$2
-CStringManager_debugRelease=$3
-CStringManager_allSame=$4
-"$CLOUD_REBUILD" CStringManager $CStringManager_bit $CStringManager_dlllib $CStringManager_debugRelease $CStringManager_allSame
-
-#--------------------------------------------------------------------
-CSystem_bit=$1
-CSystem_dlllib=$2
-CSystem_debugRelease=$3
-CSystem_allSame=$4
-"$CLOUD_REBUILD" CSystem $CSystem_bit $CSystem_dlllib $CSystem_debugRelease $CSystem_allSame
