aad432594abb218bd5a503a3f6422663b80e13cc
yangnan <yangnan@sensetime.com>
2021-04-02 17:30:48 +0800

    修改

--- src/CRandom/CRandom/src/CRandom.cpp
+++ src/CRandom/CRandom/src/CRandom.cpp
@@ -1,195 +1,68 @@
 #include "CRandom.h"
 #include <time.h>
+#include <random>
 
-class CRandomInit
-{
-public:
-	CRandomInit()
-	{
-		srand((unsigned int)time(0));
-	}
-};
-
-CRandomInit init;
-
-int CRandom::Int(const std::vector<int>& vecInt)
-{
-	return vecInt.at(Rand() % vecInt.size());
-}
+std::default_random_engine random((unsigned int)time(nullptr));
 
 int CRandom::Int(int min, int max)
 {
-	if (max - min < 0)
-	{
-		return -1;
-	}
-	return Rand() % (max - min + 1) + min;
+	std::uniform_int_distribution<int> dis(min, max);
+	return dis(random);
+}
+
+int CRandom::Int(const std::vector<int>& vecInt)
+{
+	return vecInt.at(Int(0, (int)vecInt.size() - 1));
+}
+
+double CRandom::Double(double min, double max)
+{
+	std::uniform_real_distribution<double> dis(min, max);
+	return dis(random);
 }
 
 double CRandom::Double(const std::vector<double>& vecDouble)
 {
-	return vecDouble.at(Rand() % vecDouble.size());
+	return vecDouble.at(Int(0, (int)vecDouble.size() - 1));
 }
 
-double CRandom::Double(double min, double max, int decimal)
+std::string CRandom::String(int size, unsigned char min, unsigned char max)
 {
-	if (decimal > 6 || max < min)
+	std::string result;
+	while (size-- != 0)
 	{
-		return -1;
+		result.push_back((char)Int(min, max));
 	}
-	int integer = Int((int)min, (int)max);
-	double minDecimal = min - (int)min;
-	int minDecimalInt = (int)(minDecimal * 1000000);
-	double maxDecimal = max - (int)max;
-	int maxDecimalInt = (int)(maxDecimal * 1000000);
-	int minRandomDecimal = 0;
-	int maxRandomDecimal = 999999;
-	//?说明当前得到了最小的数
-	if (integer == (int)min)
+	return result;
+}
+
+std::string CRandom::String(int minSize, int maxSize, const std::vector<std::pair<unsigned char, unsigned char>>& vecCharacter)
+{
+	std::vector<unsigned char> vecChar;
+	int index = -1;
+	while (index++ != vecCharacter.size() - 1)
 	{
-		minRandomDecimal = minDecimalInt;
+		int minChar = vecCharacter[index].first;
+		int maxChar = vecCharacter[index].second;
+		int charIndex = minChar - 1;
+		while (charIndex++ != maxChar)
+		{
+			vecChar.push_back((unsigned char)charIndex);
+		}
 	}
-	if (integer == (int)max)
+
+	int size = Int(minSize, maxSize);
+	std::string result;
+	while (size-- != 0)
 	{
-		maxRandomDecimal = maxDecimalInt;
+		result.push_back((char)vecChar.at(Int(0, (int)vecChar.size() - 1)));
 	}
-	//?最终的随机小数
-	int randomDecimal = Int(minRandomDecimal, maxRandomDecimal);
-	if (decimal == 5)
-	{
-		randomDecimal = randomDecimal / 10;
-	}
-	else if (decimal == 4)
-	{
-		randomDecimal = randomDecimal / 100;
-	}
-	else if (decimal == 3)
-	{
-		randomDecimal = randomDecimal / 1000;
-	}
-	else if (decimal == 2)
-	{
-		randomDecimal = randomDecimal / 10000;
-	}
-	else if (decimal == 1)
-	{
-		randomDecimal = randomDecimal / 100000;
-	}
-	double result;
-	if (decimal == 5)
-	{
-		result = (double)randomDecimal / 100000;
-	}
-	else if (decimal == 4)
-	{
-		result = (double)randomDecimal / 10000;
-	}
-	else if (decimal == 3)
-	{
-		result = (double)randomDecimal / 1000;
-	}
-	else if (decimal == 2)
-	{
-		result = (double)randomDecimal / 100;
-	}
-	else if (decimal == 1)
-	{
-		result = (double)randomDecimal / 10;
-	}
-	result = result + (double)integer;
 	return result;
 }
 
 std::string CRandom::String(const std::vector<std::string>& vecString)
 {
-	return vecString.at(Rand() % vecString.size());
-}
-
-std::string CRandom::String(int nMin, int nMax, bool ifHasZero)
-{
-	if (nMin < 0 || nMax < 0 || nMax - nMin < 0)
-	{
-		return "";
-	}
-	std::vector<char> vecCharacter;
-	char i = -128;
-	while (i++ != 127)
-	{
-		if (ifHasZero == false && i == 0)
-		{
-			continue;
-		}
-		vecCharacter.push_back(i);
-	}
-	vecCharacter.push_back(-128);
-	return String(nMin, nMax, vecCharacter);
-}
-
-std::string CRandom::String(int nMin, int nMax, const std::vector<char>& vecCharacter)
-{
-	if (nMin < 0 || nMax < 0 || nMax - nMin < 0)
-	{
-		return "";
-	}
-
-	int nLength = Rand() % (nMax - nMin + 1) + nMin;
-	int nSize = vecCharacter.size();
-	std::string strResult;
-	strResult.resize(nLength);
-	int i = -1;
-	while (i++ != nLength - 1)
-	{
-		strResult[i] = vecCharacter.at(Rand() % nSize);
-	}
-	return strResult;
-}
-
-std::string CRandom::String(int nMin, int nMax, int enumFlag)
-{
-	std::vector<char> vecChar;
-	//?说明有小写参与
-	if ((enumFlag & 0x00000001) == 0x00000001)
-	{
-		int i = 96;
-		while (i++ != 122)
-		{
-			vecChar.push_back(i);
-		}
-	}
-	if ((enumFlag & 0x00000010) == 0x00000010)
-	{
-		int i = 64;
-		while (i++ != 90)
-		{
-			vecChar.push_back(i);
-		}
-	}
-	if ((enumFlag & 0x00000100) == 0x00000100)
-	{
-		int i = 48;
-		while (i++ != 57)
-		{
-			vecChar.push_back(i);
-		}
-	}
-	if ((enumFlag & 0x00001000) == 0x00001000)
-	{
-		vecChar.push_back(48);
-	}
-	std::string strResult = String(nMin, nMax, vecChar);
-	if ((enumFlag & 0x00010000) == 0x00010000)
-	{
-		for (auto itvec = vecChar.begin(); itvec != vecChar.end(); itvec++)
-		{
-			if (*itvec == 48)
-			{
-				vecChar.erase(itvec);
-				break;
-			}
-		}
-		strResult[0] = String(1, 1, vecChar)[0];
-	}
-	return strResult;
+	return vecString.at(Int(0, (int)vecString.size() - 1));
 }
 
 std::vector<std::string> CRandom::Deal(const std::map<std::string, int>& mapCard)
@@ -204,7 +77,7 @@ std::vector<std::string> CRandom::Deal(const std::map<std::string, int>& mapCard
 			vecCard.push_back(itmapCard->first);
 		}
 	}
-	int i = vecCard.size();
+	size_t i = vecCard.size();
 	while (i-- != 0)
 	{
 		std::string card = String(vecCard);
@@ -237,81 +110,6 @@ std::vector<int> CRandom::Hash(int hashNumber, int size)
 		}
 	}
 	return vecResult;
-}
-
-std::string CRandom::toBit(unsigned int number)
-{
-	std::string strResult = "00000000000000000000000000000000";
-	int nBit = 32;
-	while (nBit-- != 0)
-	{
-		if (number % 2 == 1)
-		{
-			strResult[nBit] = '1';
-		}
-		number = number / 2;
-		if (number == 0)
-		{
-			break;
-		}
-	}
-	return strResult;
-}
-
-unsigned int CRandom::toUnsignedInt(const std::string& bit)
-{
-	unsigned int nBit = 1;
-	unsigned int result = 0;
-	int i = 32;
-	while (i-- != 0)
-	{
-		result = result + (bit[i] - 48) * nBit;
-		nBit = nBit * 2;
-	}
-	return result;
-}
-
-int CRandom::GetBigBit(const std::string& bit1, const std::string& bit2)
-{
-	int i = -1;
-	while (i++ != 31)
-	{
-		if (bit1[i] != bit2[i])
-		{
-			break;
-		}
-	}
-	return i;
-}
-
-int CRandom::GetSmallBit(const std::string& bit1, const std::string& bit2)
-{
-	int i = 32;
-	while (i-- != 0)
-	{
-		if (bit1[i] != bit2[i])
-		{
-			break;
-		}
-	}
-	return i;
-}
-
-std::string CRandom::RandomBitString(int nBit)
-{
-	std::string strResult;
-	strResult.resize(nBit);
-	int i = nBit;
-	while (i-- != 0)
-	{
-		strResult[i] = rand() % 2 + 48;
-	}
-	return strResult;
-}
-
-unsigned int CRandom::Rand()
-{
-	return toUnsignedInt(RandomBitString(32));
 }
 
 /*
--- src/CRandom/CRandom/src/CRandom.h
+++ src/CRandom/CRandom/src/CRandom.h
@@ -6,41 +6,22 @@
 class CRandomAPI CRandom
 {
 public:
-	enum
-	{
-		smallLetter = 0x00000001,
-		bigLetter = 0x00000010,
-		numberNoZero = 0x00000100,
-		zero = 0x00001000,
-		firstNoZero = 0x00010000
-	};
-public:
-	//?从以下数中随机产生一个数
-	static int Int(const std::vector<int>& vecInt);
 	//?从以下范围中产生一个随机数，包含头尾，产生不出返回-1，范围上限21亿
 	static int Int(int min, int max);
+	//?从以下数中随机产生一个数
+	static int Int(const std::vector<int>& vecInt);
+	//?产生一个浮点数，左闭右开
+	static double Double(double min, double max);
 	//?从以下数中产生一个随机小数
 	static double Double(const std::vector<double>& vecDouble);
-	//?产生一个浮点数，传入的小数最多六位，最后一个传小数位，包含头尾，产生不出则返回-1
-	static double Double(double min, double max, int decimal);
+	//?生成固定长度的字符串，如果不带0则为1, 255
+	static std::string String(int size, unsigned char min, unsigned char max);
+	//?产生一个随机字符串，最小长度，最大长度，字符串中可能出现的字符，左小右大
+	static std::string String(int minSize, int maxSize, const std::vector<std::pair<unsigned char, unsigned char>>& vecCharacter);
 	//?随机选出一个字符串
 	static std::string String(const std::vector<std::string>& vecString);
-	//?从所有字符中随机出一串，默认生成字符串中含0
-	static std::string String(int nMin, int nMax, bool ifHasZero = true);
-	//?产生一个随机字符串，最小长度，最大长度，字符串中可能出现的字符，如果不传则为任何字符
-	static std::string String(int nMin, int nMax, const std::vector<char>& vecCharacter);
-	//?产生一个字符串，用|符号来合并可能出现的字符，见enum
-	static std::string String(int nMin, int nMax, int enumFlag);
-	//?发牌
+	//?洗牌
 	static std::vector<std::string> Deal(const std::map<std::string, int>& mapCard);
-	//?散列
+	//?散列，可用于发牌
 	static std::vector<int> Hash(int hashNumber, int size);
-
-private:
-	static std::string toBit(unsigned int number);
-	static unsigned int toUnsignedInt(const std::string& bit);
-	static int GetBigBit(const std::string& bit1, const std::string& bit2);
-	static int GetSmallBit(const std::string& bit1, const std::string& bit2);
-	static std::string RandomBitString(int nBit);
-	static unsigned int Rand();
 };
