596ef11150f13476f7722b65817986108ff8aa57
xueyelingshuang <yangnan123456789@163.com>
2021-05-10 01:35:32 +0800

    修改

--- src/Dtws/Dtws/SkillTask.cpp
+++ src/Dtws/Dtws/SkillTask.cpp
@@ -12,12 +12,17 @@ m_exit(false)
 void SkillTask::DoTask()
 {
 	std::string currentExePath = CSystem::GetCurrentExePath();
+	int32_t index = -1;
 	while (!m_exit)
 	{
 		CKeyboard::InputString("\t");
 		Sleep(200);
 		ScreenScript::FindClick(currentExePath + "res\\shoufu.png", true, false, xyls::Rect(xyls::Point(904, 557), 96, 80));
 		Sleep(1000);
+		if (++index % 10 == 0)
+		{
+			CKeyboard::InputString("`");
+		}
 	}
 }
 
