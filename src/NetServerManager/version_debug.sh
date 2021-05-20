4da7a8f45c8070711ddae6dfdff88dbbf0074905
yangnan <yangnan@sensetime.com>
2021-05-20 20:50:41 +0800

    修改

--- src/NetServerManager/version_debug.sh
+++ src/NetServerManager/version_debug.sh
@@ -1,5 +1,4 @@
-"$CLOUD_REBUILD" ProtoMessage 32 dll debug same
+"$CLOUD_REBUILD" NetSender 32 dll debug same
 "$CLOUD_REBUILD" NetWork 32 dll debug same
-"$CLOUD_REBUILD" ProcessWork 32 dll debug same
 "$CLOUD_REBUILD" CDump 32 dll debug same
 "$CLOUD_REBUILD" Compress 32 dll debug same
--- src/NetServerManager/version_release.sh
+++ src/NetServerManager/version_release.sh
@@ -1,5 +1,4 @@
-"$CLOUD_REBUILD" ProtoMessage 32 lib release same
+"$CLOUD_REBUILD" NetSender 32 lib release same
 "$CLOUD_REBUILD" NetWork 32 lib release same
-"$CLOUD_REBUILD" ProcessWork 32 lib release same
 "$CLOUD_REBUILD" CDump 32 lib release same
 "$CLOUD_REBUILD" Compress 32 lib release same
