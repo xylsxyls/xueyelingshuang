68385da52ea00d3a4f25bf67211b01c5ef1b0d89
xueyelingshuang <yangnan123456789@163.com>
2021-06-10 23:18:56 +0800

    修改

--- src/Dtws/Dtws/SkillTask.cpp
+++ src/Dtws/Dtws/SkillTask.cpp
@@ -17,13 +17,16 @@ void SkillTask::DoTask()
 	int32_t index = -1;
 	while (!m_exit)
 	{
-		CKeyboard::InputString("\t");
-		Sleep(200);
+		xyls::Point currentPos = CMouse::GetCurrentPos();
+		if (lastPos.x() == currentPos.x() && lastPos.y() == currentPos.y())
+		{
+			CKeyboard::InputString("\t");
+			Sleep(200);
+		}
 		ScreenScript::FindClick(currentExePath + "res\\shoufu.png", true, false, xyls::Rect(xyls::Point(904, 557), 96, 80));
 		Sleep(1000);
 		if (++index % 5 == 0)
 		{
-			xyls::Point currentPos = CMouse::GetCurrentPos();
 			if (lastPos.x() == currentPos.x() && lastPos.y() == currentPos.y())
 			{
 				CMouse::MoveAbsolute(xyls::Point(817, 13), 0);
