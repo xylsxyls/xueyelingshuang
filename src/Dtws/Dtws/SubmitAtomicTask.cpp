1606004442f79863ffbd620a341f3dbe96f75539
xylsxyls <yangnan123456789@163.com>
2023-05-02 18:09:50 +0800

    修改

--- src/Dtws/Dtws/SubmitAtomicTask.cpp
+++ src/Dtws/Dtws/SubmitAtomicTask.cpp
@@ -1,6 +1,8 @@
 #include "SubmitAtomicTask.h"
 #include "CMouse/CMouseAPI.h"
 #include "Config.h"
+#include "DtwsParam.h"
+#include "CSystem/CSystemAPI.h"
 
 SubmitAtomicTask::SubmitAtomicTask():
 m_lineIndex(0)
@@ -10,7 +12,10 @@ m_lineIndex(0)
 
 bool SubmitAtomicTask::DoFun(const std::vector<std::pair<std::string, xyls::Rect>>& vecPic)
 {
-	Sleep(500);
+	if (CSystem::getComputerName() != FIRST_COMPUTER)
+	{
+		Sleep(100);
+	}
 	CMouse::MoveAbsolute(g_screen.m_submitPoint[m_accountIndex] + xyls::Point(0, m_lineIndex * g_config.m_textLineHeight), 0);
 	CMouse::LeftClick(0);
 	return false;
