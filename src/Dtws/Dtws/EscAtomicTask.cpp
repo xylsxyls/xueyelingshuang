afd3ef48f9917b8ed45ed07b1b8b7ace28b9689d
xylsxyls <yangnan123456789@163.com>
2025-07-12 21:24:35 +0800

    修改

--- src/Dtws/Dtws/EscAtomicTask.cpp
+++ src/Dtws/Dtws/EscAtomicTask.cpp
@@ -1,8 +1,10 @@
 #include "EscAtomicTask.h"
 #include "CKeyboard/CKeyboardAPI.h"
+#include "Config.h"
 
 bool EscAtomicTask::DoFun(const std::vector<std::pair<std::string, xyls::Rect>>& vecPic)
 {
+	Sleep(g_config.m_goFirstSleepTime);
 	CKeyboard::KeyDown(CKeyboard::Esc);
 	CKeyboard::KeyUp(CKeyboard::Esc);
 	return false;
