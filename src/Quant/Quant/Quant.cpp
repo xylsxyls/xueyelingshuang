57df37b01cdca1ef8fa0a23ddd03a3e442ccf4e9
xylsxyls <yangnan123456789@163.com>
2025-07-15 17:01:30 +0800

    提交

---
+++ src/Quant/Quant/Quant.cpp
@@ -0,0 +1,54 @@
+﻿#include "Quant.h"
+//#include "11Controls/controls/COriginalButton.h"
+//#include "11Controls/controls/DialogManager.h"
+
+Quant::Quant(QWidget* parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	//m_button = new COriginalButton(this);
+	init();
+}
+
+Quant::~Quant()
+{
+
+}
+
+void Quant::init()
+{
+	if (!check())
+	{
+		return;
+	}
+	QPalette pattle;
+	pattle.setColor(QPalette::Background, QColor(100, 0, 0, 255));
+	setPalette(pattle);
+	//m_button->setBkgColor(QColor(255, 0, 0, 255), QColor(0, 255, 0, 255), QColor(0, 0, 255, 255), QColor(255, 0, 0, 255));
+	//QObject::connect(m_button, &COriginalButton::clicked, this, &Quant::onButtonClicked);
+}
+
+bool Quant::check()
+{
+	return true;
+	return m_button != nullptr;
+}
+
+void Quant::resizeEvent(QResizeEvent* eve)
+{
+	QMainWindow::resizeEvent(eve);
+
+	if (!check())
+	{
+		return;
+	}
+	//m_button->setGeometry(360, 120, 160, 80);
+}
+
+void Quant::onButtonClicked()
+{
+	//TipDialogParam tipDialogParam;
+	//tipDialogParam.m_tip = QStringLiteral("点击");
+	//tipDialogParam.m_parent = windowHandle();
+	//DialogManager::instance().makeDialog(tipDialogParam);
+}
---
+++ src/Quant/Quant/Quant.h
@@ -0,0 +1,31 @@
+﻿#ifndef QTTEST_H
+#define QTTEST_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_Quant.h"
+
+class COriginalButton;
+
+class Quant : public QMainWindow
+{
+	Q_OBJECT
+public:
+	Quant(QWidget* parent = nullptr);
+	~Quant();
+
+protected:
+	void init();
+	bool check();
+
+protected:
+	void resizeEvent(QResizeEvent* eve);
+
+private slots:
+	void onButtonClicked();
+
+private:
+	Ui::QuantClass ui;
+	COriginalButton* m_button;
+};
+
+#endif // QTTEST_H
